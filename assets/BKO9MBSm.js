b940f8055add5860d092e4d097494945705e1aec
André Duarte <andrexduarte@gmail.com>
2024-06-26 19:07:27 -0300

    854e8a6

---
+++ assets/BKO9MBSm.js
@@ -0,0 +1,1 @@
+import{aa as r}from"./DIsRfJGq.js";import{u as a,a as u}from"./BcSIsjWd.js";const l=r((o,m)=>{const t=a(),e=u.get("curriculumTemplate");e&&(t.currentTemplate=e)});export{l as default};
---
+++ assets/CyOdNijh.js
@@ -0,0 +1,1 @@
+import{p as o}from"./B_-3eTQM.js";import"./B2LprFBL.js";import"./DIsRfJGq.js";import"./B9ot-Zm_.js";import"./BcSIsjWd.js";const a={locales:{pt:o},locale:"pt"};export{a as default};
