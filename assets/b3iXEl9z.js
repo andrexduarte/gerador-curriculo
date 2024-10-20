4a3f351c8ac7ebeeb6b6f3eb6f8021a28e2d6895
André Duarte <andrexduarte@gmail.com>
2024-10-20 18:03:42 -0300

    9803746

---
+++ assets/b3iXEl9z.js
@@ -0,0 +1,1 @@
+import{p as o}from"./CB6SHSZ8.js";import"./DPzj7Xjd.js";import"./C7lKBqEH.js";import"./B9-XJhwM.js";const m={locales:{pt:o},locale:"pt"};export{m as default};
---
+++ assets/jps6U0fR.js
@@ -0,0 +1,1 @@
+import{u as r,a as u}from"./B9-XJhwM.js";import{a7 as a}from"./DPzj7Xjd.js";const l=a((o,m)=>{const t=r(),e=u.get("curriculumTemplate");e&&(t.currentTemplate=e)});export{l as default};
